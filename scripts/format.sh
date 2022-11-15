8e8fe95058da52e2384e1445e88b32a23d707e4f
Felix Altenberger <felix@zenml.io>
2022-11-15 05:58:12 +0100

    Revert "Update README and pyproject.toml and remove ZenBytes stuff"
    This reverts commit 5162cf8ccea954b71b10f930a40c424195e85429.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 set -x
 
-SRC=${1:-"run*.py steps/ pipelines/"}
+SRC=${1:-"run.py steps/ pipelines/"}
+NOTEBOOKS=${@:-"*.ipynb"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py
 isort $SRC
-black $SRC
+black $SRC $NOTEBOOKS
