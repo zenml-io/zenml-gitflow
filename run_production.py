e3f22cd5e71ddc053674a8d26c2568b517f334fa
Hamza Tahir <htahir111@gmail.com>
2022-11-16 10:23:28 +0100

    Caching virtualenv 3

--- run_production.py
+++ run_production.py
@@ -39,7 +39,7 @@ def main():
         importer=production_data_loader(),
         trainer=svc_trainer_mlflow(
             params=TrainerParams(
-                degree=10,
+                degree=15,
             )
         ).configure(experiment_tracker=experiment_tracker.name),
         evaluator=evaluator(),
