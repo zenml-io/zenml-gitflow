30b20d6dbeccd4586b248849a61d47f2d25d6226
Stefan Nica <stefan@zenml.io>
2023-01-19 14:36:34 +0100

    Minor workflow fixes

--- utils/tracker_helper.py
+++ utils/tracker_helper.py
@@ -23,7 +23,7 @@ def get_tracker_name() -> Optional[str]:
     """Get the name of the active experiment tracker."""
 
     experiment_tracker = Client().active_stack.experiment_tracker
-    return experiment_tracker.name
+    return experiment_tracker.name if experiment_tracker else None
 
 
 def enable_autolog() -> None:
